bd956cd7a04dc72e32c49d24a05ede288a52d7e8
jiseong7278 <67218734+jiseong7278@users.noreply.github.com>
2024-08-30 16:35:12 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,8 @@
+def solution(my_string, overwrite_string, s):
+    answer = ''
+    
+    answer += my_string[:s]
+    answer += overwrite_string
+    answer += my_string[s+len(overwrite_string):len(my_string)]
+    
+    return answer

0b3837782b5e0b9aca91e70d6fd9d0221561408e
jiseong7278 <67218734+jiseong7278@users.noreply.github.com>
2024-09-03 03:04:27 +0900

    [Silver IV] Title: 스택 2, Time: 1192 ms, Memory: 71112 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,28 @@
+import sys
+
+
+N = int(sys.stdin.readline())
+
+stack = []
+
+for i in range(N):
+    command = list(map(int, sys.stdin.readline().split()))
+
+    if command[0] == 1:
+        stack.append(command[1])
+    elif command[0] == 2:
+        if len(stack) > 0:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == 3:
+        print(len(stack))
+    elif command[0] == 4:
+        print(1 if len(stack) == 0 else 0)
+    elif command[0] == 5:
+        if len(stack) > 0:
+            temp = stack.pop()
+            print(temp)
+            stack.append(temp)
+        else:
+            print(-1)

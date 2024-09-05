5b74d7be6eda785befb4e0c3f12dd6273bf1ae3a
jiseong7278 <67218734+jiseong7278@users.noreply.github.com>
2024-09-05 20:36:18 +0900

    [level 2] Title: 디펜스 게임, Time: 736.40 ms, Memory: 45.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -0,0 +1,15 @@
+import heapq
+
+def solution(n, k, enemy):
+    answer = 0
+
+    heap = []
+    
+    for i, e in enumerate(enemy):
+        heapq.heappush(heap, e)
+        if len(heap) > k:
+            n -= heapq.heappop(heap)
+        if n < 0:
+            return i
+    
+    return len(enemy)

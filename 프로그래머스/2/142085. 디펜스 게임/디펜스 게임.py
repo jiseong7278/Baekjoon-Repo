7c531a5c44dbdafdbdb8f983d1accaf9fb9beb50
jiseong7278 <67218734+jiseong7278@users.noreply.github.com>
2024-09-05 20:41:31 +0900

    Update 디펜스 게임.py
    주석 추가

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -8,8 +8,8 @@ def solution(n, k, enemy):
     for i, e in enumerate(enemy):
         heapq.heappush(heap, e)
         if len(heap) > k:
-            n -= heapq.heappop(heap)
-        if n < 0:
+            n -= heapq.heappop(heap)  # 제일 낮은 값을 뽑은 뒤 n에서 해당 값을 뺌
+        if n < 0:  # 배열의 0 ~ i까지의 요소들 중 낮은 값들을 빼고 n < 0 이 되면 Game Over
             return i
     
     return len(enemy)

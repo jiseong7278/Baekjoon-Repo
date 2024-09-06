c08bd82cf7c2927a90142c63074a9cfae6bac1b9
jiseong7278 <67218734+jiseong7278@users.noreply.github.com>
2024-09-07 02:41:41 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,18 +1,7 @@
 def solution(n, arr1, arr2):
     answer = []
     
-    for i in range(len(arr1)):
-        temp = ''
-        
-        str_1 = str(bin(arr1[i])[2:].zfill(n))
-        str_2 = str(bin(arr2[i])[2:].zfill(n))
-        
-        for j in range(len(str_1)):
-            if str_1[j] == '1' or str_2[j] == '1':
-                temp += '#'
-            else:
-                temp += ' '
-                
-        answer.append(temp)
+    for a, b in zip(arr1, arr2):
+        answer.append(bin(a|b)[2:].zfill(n).replace("0", " ").replace("1", "#"))
     
     return answer

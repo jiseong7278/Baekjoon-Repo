e515b72f52bf83472610f7ce3ac3b3e41082b9cf
jiseong7278 <67218734+jiseong7278@users.noreply.github.com>
2024-10-24 00:42:39 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 564 ms, Memory: 68780 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,36 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+
+N, M, R = map(int, sys.stdin.readline().rstrip().split())
+
+nodes = [[] for i in range(N+1)]
+visited = [False] * len(nodes)
+answer = [0 for _ in range(N+1)]
+
+visit_order = 1
+
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().rstrip().split())
+    nodes[u].append(v)
+    nodes[v].append(u)
+
+for i in range(N+1):
+    nodes[i].sort(reverse=True)
+
+    
+def DFS(start):
+    global visit_order
+
+    visited[start] = True
+    answer[start] = visit_order
+    visit_order += 1
+
+    for n in nodes[start]:
+        if not visited[n]:
+            DFS(n)
+
+DFS(R)
+
+for i in range(1, N+1):
+    print(answer[i])
